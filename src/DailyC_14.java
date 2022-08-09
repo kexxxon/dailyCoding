316b755811ffa323b89ddd2f45fbe15c4d9d01ce
kexxxon <dev.kexon@gmail.com>
2022-08-09 23:12:50 +0900

    dailyCoding_14_superIncreasing

--- src/DailyC_14.java
+++ src/DailyC_14.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DailyC_14 {
+public class DailyC_14 {
+    public static boolean superIncreasing(int[] arr) {
+        if(arr.length == 0) return false;
+        int sum = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] <= sum) return false;
+            sum += arr[i];
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(superIncreasing(new int[]{1, 3, 6, 13, 54})); // true
+        System.out.println(superIncreasing(new int[]{612, 1226})); // true
+        System.out.println(superIncreasing(new int[]{1, 2, 4, 8, 15})); // true
+    }
 }
