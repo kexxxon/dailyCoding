95c3ca9b97f7e05b6155f338b361ca176464c454
kexxxon <dev.kexon@gmail.com>
2022-08-23 00:28:00 +0900

    dailyCoding_19_decryptCipher

--- src/DailyC_19.java
+++ src/DailyC_19.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class DailyC_19 {
+public class DailyC_19 {
+    public static String decryptCipher(String str, int secret) {
+
+        String alphabet ="abcdefghijklmnopqrstuvwxyz";
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ' ') {
+                result += str.charAt(i);
+            } else {
+                int asis = alphabet.indexOf(str.charAt(i));
+                int tobe = (asis - secret + alphabet.length()) % alphabet.length();
+                result += alphabet.charAt(tobe);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decryptCipher("khoor", 3));
+    }
 }
