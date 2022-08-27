7291144a784bdcc3b633c75aaec398f25d1bb41b
kexxxon <dev.kexon@gmail.com>
2022-08-27 18:44:28 +0900

    refactor: dailyCoding_25_power

--- DailyC_25.java
+++ DailyC_25.java
@@ -1,7 +1,9 @@
 public class DailyC_25 {
+    final static int MOD = 94_906_249;
     public static long power(int base, int exponent) {
         if(exponent == 0) return 1;
 
+    /*
         int half = exponent / 2;
         long temp = power(base, half);
         long result = (temp * temp) % 94906249;
@@ -10,6 +12,15 @@ public class DailyC_25 {
             return (base * result) % 94906249;
         else
             return result;
+    */
+
+        long half = power(base, exponent / 2);
+        long result = half * half % MOD;
+
+        if(exponent % 2 == 1)
+            return result * base % MOD;
+
+        return result;
     }
 
     public static void main(String[] args) {
